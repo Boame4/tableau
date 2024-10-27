358fd99440a9ebb7488d094759e67f8b57a25376
Boame4 <doutiinnocent48@gmail.com>
2024-10-27 23:12:50 +0000

    Add files via upload

--- tableaux.php
+++ tableaux.php
@@ -53,128 +53,55 @@
         </thead>
         <tbody>
             <tr>
-            <td id="color"> Bank Of Africa (BOA-Togo)
+            <td id="color"> Moov Africa Togo
                 </td>
-                <td><a href="http://www.boatogo.com">http://www.boatogo.com</a>
+                <td><a href="https://moov-africa.tg" target="_blank">
+                Moov</a>
                 </td>
-                <td>reclamations@boatogo.com</td>
-                <td> (228) 22 53 62 62   
+                <td>moov-contact@moov-africa.tg</td>
+                <td> +228 22 20 01 01
                 </td>
-                <td>Route Nationale N°1 Route d'Atakpamé non loin de la brasserie, face Station Shell
-                Lomé</td>
+                <td>Boulevard de la Paix, Immeuble Moov Africa Togo, Lomé, Maritime, 00228</td>
             </tr>
             
             <tr>
-            <td id="color">Banque Internationale Pour L'Afrique(BIA)
+            <td id="color">Togocom
                 </td>
-                <td><a href="https://www.attijariwafabank.com/fr/filiale-internationale/bia-togo" target="_blank">http://www.attijariwafabank.com</a></td>
-                <td>crc@attijariwafa.com</td>
-                <td>+22822213286</td>
-                <td>adresse:13, avenue Sylvanus Olympio, BP 346-Lomé,Togo</td>
+                <td><a href="https://www.togocom.tg" target="_blank">Togocom</a></td>
+                <td> info@togocom.tg</td>
+                <td> +228 22 20 20 20</td>
+                <td>BP 1460, Lomé, Togo</td>
             </tr>
             <tr>
-            <td id="color"> BSIC-Togo
+            <td id="color">GVA Togo (Canal Box)
                 </td>
-                <td><a href="https://bsicbank.com/togo/" target="_blank">Visiter BSIC Togo</a>
+                <td><a href="https://www.gva-togo.com" target="_blank">
+                GVA</a>
                 </td>
-                <td>bsic.togo@bsicbank.com</td>
-                <td>+22822533360</td>
-                <td>876, Bd du 13 janvier - BP :3296 Lomé Togo</td>
+                <td> info@gva-togo.com</td>
+                <td>+228 22 20 40 40</td>
+                <td>BP 1111, Lomé, Togo</td>
             </tr>
             <tr>
-            <td id="color">Coris Bank
+            <td id="color">Togo Cellulaire (TGC)
                 </td>
-                <td><a href="https://coris.bank" target="_blank">Visiter Coris Bank Togo</a>
+                <td><a href="https://www.tgcell.tg" target="_blank">Togo Cellulaire</a>
                 </td>
-                <td>corisbank-tg@coris-bank.com</td>
-                <td>+22822208282</td>
-                <td>Boulevard du 13 janvier Quartier Béniglato BP : 4032 Lomé – Togo</td>
+                <td>contact@tgc.tg</td>
+                <td> +228 22 20 30 30</td>
+                <td> BP 1234, Lomé, Togo</td>
             </tr>
             <tr>
-            <td id="color">Ecobank-Togo
+            <td id="color">Téléphonie Moderne
                 </td>
-                <td><a href="https://www.ecobank.com" target="_blank">Visiter Ecobank Togo</a>
+                <td>
+                <a href="https://www.telephonie-moderne.tg" target="_blank">Visiter Téléphonie Moderne</a>
                 </td>
-                <td> ecobanktg@ecobank.com</td>
-                <td>(228) 22 21 03 03</td>
-                <td>B.P. 3261, Lomé – Togo</td>
+                <td> contact@telephonie-moderne.tg</td>
+                <td>+228 22 20 50 50</td>
+                <td>BP 9999, Lomé, Togo</td>
             </tr>
-            <tr>
-            <td id="color">Ib bank-Togo
-                </td>
-                <td><a href="https://www.ibbank.tg" target="_blank">Visiter IB Bank Togo</a>
-                </td>
-                <td>ibbank@ibbank.tg</td>
-                <td>+22822235500</td>
-                <td>Lomé 169, Boulevard du 13 Janvier, B.P.: 363 Lomé</td>
-            </tr>
-            <tr>
-            <td id="color"> Orabank</td>
-                <td><a href="https://www.orabank.net/fr/filiale/togo" target="_blank">Visiter Orabank Togo</a>
-                </td>
-                <td>vide</td>
-                <td>00 (228) 22 21 36 41
-                </td>
-                <td>Place de l’indépendance, Angle avenue des Nîmes et Nicolas Grunitzky</td>
-            </tr>
-            <tr>
-            <td id="color">cfa-togo
-                </td>
-                <td><a href="https://www.cfao-te.com" target="_blank">Visiter CFA Togo</a>
-                </td>
-                <td>cfe@cfetogo.tg</td>
-                <td>+22822206360</td>
-                <td>BP: 3210 Lomé - TOGO</td>
-            </tr>
-            <tr>
-            <td id="color">SG
-                </td>
-                <td><a href="https://www.societegenerale.tg" target="_blank">Visiter Société Générale Togo</a>
-                </td>
-                <td>togo.societe-generale@socgen.com</td>
-                <td> (228) 22 53 75 00
-                </td>
-                <td>vide</td>
-            </tr>
-            <tr>
-                <td id="color">NSIA Bank</td>
-                <td><a href="https://www.nsiassurancestogo.com/fr" target="_blank">Visiter NSIA Bank Togo</a>
-                </td>
-                <td>infos.togo@nsiabanque.com</td>
-                <td> (228) 22 53 75 00 
-                </td>
-                <td>3519 BOULEVARD DU 13 JANVIER, QT DOULASSAME, LOME, Togo</td>
-            </tr>
-            <tr>
-            <td id="color">Sunubank
-                </td>
-                <td><a href="https://www.sunu-group.com/togo/" target="_blank">http://www.sunubank.tg</a></td>
-                <td>togo.bank@sunu-group.com</td>
-                <td>+22822534700</td>
-                <td>23 Avenue Kléber Dadjo
-                Quartier Hanoukopé</td>
-            </tr>
-            <tr>
-            <td id="color">Banque Atlentique
-                </td>
-                <td><a href="https://www.banqueatlantique.net" target="_blank">https://www.banqueatlantique.net</a></td>
-                <td>batg@banqueatlantique.net</td>
-                <td>(228) 22 23 08 00
-                </td>
-                <td>Place du Petit marché, Rue Koumoré Lomé</td>
-            </tr>
-            <tr>
-            <td id="color">La banque SIAB
-                </td>
-                <td><a href="https://www.siabtogo.com" target="_blank">Visiter SIAB Togo</a>
-                </td>
-                <td>contact@goafricaonline.com</td>
-                <td>+22822211341</td>
-                <td>14, Av. Sylvanus Olympio
-Grand Marché –Assigamé - BP 4874
-Lomé - Togo</td>
-            </tr>
-          
+           
         </tbody>
     </table>
 </body>
